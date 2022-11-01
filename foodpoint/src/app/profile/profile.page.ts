46107d02a55a4a791efee9302025e64b3b7a44a4
igormassami9 <igormassami9@gmail.com>
2022-11-01 16:24:16 -0300

    adicionado estruturas nas paginas, relação de firebase na página profile.

--- foodpoint/src/app/profile/profile.page.ts
+++ foodpoint/src/app/profile/profile.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from "../shared/authentication-service";
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
 
 @Component({
   selector: 'app-profile',
@@ -7,7 +11,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public authService: AuthenticationService, 
+    public router:Router,
+    public auth: AngularFireAuth
+    ) { }
 
   ngOnInit() {
   }
